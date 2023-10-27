5cc828be2903963d242673f2aad4cb85e5ed1a11
Yousef Khell <yousef@khell.com>
2023-10-28 00:04:10 +0200

    Erster Isomorphiesatz

---
+++ src/.auctex-auto/algebra-1-skript.el
@@ -0,0 +1,40 @@
+(TeX-add-style-hook
+ "algebra-1-skript"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "a4paper")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "amsfonts"
+    "amssymb"
+    "amsmath"
+    "amsthm"
+    "graphicx"
+    "enumerate"
+    "faktor"
+    "tikz-cd")
+   (LaTeX-add-labels
+    "sec:Gruppen und Monoide"
+    "sec:Gruppenhomomorphismen")
+   (LaTeX-add-amsthm-newtheorems
+    "thm"
+    "lemm"
+    "prop"
+    "satz"
+    "kor"
+    "cor"
+    "defi"
+    "rem"
+    "bem"
+    "exmp"
+    "bsp"
+    "Conc"
+    "auf"
+    "ubng"
+    "loes"
+    "exer"
+    "soln"))
+ :latex)
+
