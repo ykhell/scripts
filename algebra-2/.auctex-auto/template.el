36ef092175834459bae5da1103af146bb87cd9fc
Yousef Khell <yousef@khell.com>
2024-04-23 16:53:28 +0200

    Lecture 2

--- algebra-2/.auctex-auto/template.el
+++ algebra-2/.auctex-auto/template.el
@@ -1,6 +1,13 @@
 (TeX-add-style-hook
  "template"
  (lambda ()
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "amsfonts"
     "amssymb"
@@ -19,6 +26,8 @@
     "hyperref")
    (TeX-add-symbols
     '("mfaktor" ["argument"] 2)
+    '("fak" 2)
+    '("nb" 1)
     '("eb" 1)
     "N"
     "Nn"
@@ -38,7 +47,6 @@
     "ex"
     "imp"
     "impb"
-    "fak"
     "nt"
     "ent"
     "tn"
@@ -49,6 +57,7 @@
     "T"
     "lm"
     "mps"
+    "m"
     "mpsfrom"
     "tm"
     "en"
@@ -59,7 +68,7 @@
     "cld"
     "mfaktor"
     "bigtimes")
-   (LaTeX-add-environments
+   (LaTeX-add-amsthm-newtheorems
     "thm"
     "lemm"
     "prop"
@@ -77,6 +86,7 @@
     "exmp"
     "bsp"
     "Conc"
+    "nota"
     "whg"
     "auf"
     "ubng"
