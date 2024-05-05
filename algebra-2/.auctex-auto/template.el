f6cf9fdfb4098cda1c2ee0e676592328bf9ecb79
Yousef Khell <yousef@khell.com>
2024-05-05 17:25:07 +0200

    Up to date 3.5.24

--- algebra-2/.auctex-auto/template.el
+++ algebra-2/.auctex-auto/template.el
@@ -1,12 +1,12 @@
 (TeX-add-style-hook
  "template"
  (lambda ()
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "amsfonts"
@@ -42,6 +42,7 @@
     "Rn"
     "skl"
     "G"
+    "U"
     "und"
     "ubr"
     "obr"
