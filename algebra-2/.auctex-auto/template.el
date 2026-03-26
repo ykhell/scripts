ec3ff6e3a5fd117a9fde29cf166c57e24fe93148
Yousef Khell <yousef@khell.com>
2026-03-26 16:49:56 +0100

    Add sociology

--- algebra-2/.auctex-auto/template.el
+++ algebra-2/.auctex-auto/template.el
@@ -1,12 +1,12 @@
 (TeX-add-style-hook
  "template"
  (lambda ()
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "amsfonts"
@@ -82,6 +82,10 @@
     "tot"
     "inj"
     "proj"
+    "Spec"
+    "Max"
+    "Min"
+    "Zar"
     "ex"
     "imp"
     "nimp"
